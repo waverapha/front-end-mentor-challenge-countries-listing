efffb13ba31581db27148adeeba23a1fe9077a60
Raphael <dasads>
2021-01-20 18:50:49 -0300

    BaseSelect agora pode ter opções selecionadas pelas setas do teclado

--- src/components/BaseSelect.vue
+++ src/components/BaseSelect.vue
@@ -23,15 +23,16 @@
     :aria-label="`${selected.name} / Use TAB to navigate`">
 
       <li
-      :tabindex="(activeList) ? 0 : -1"
       class="base-select-list-item"
       :class="listOptionClass"
       v-for="option in selectOptions"
       :key="option.id"
       role="option">
         <a
+        :tabindex="(activeList) ? 0 : -1"
         class="base-select-list-item-changer"
-        @click="changeChoosedOption(option)">{{ option.name }}</a>
+        @click="changeChoosedOption(option)"
+        @keydown.enter="changeChoosedOption(option)">{{ option.name }}</a>
       </li>
 
     </ul>
@@ -76,6 +77,8 @@ export default {
     },
 
     mounted(){
+      this.list = this.$el.querySelectorAll('#base-select li a');
+
       document.addEventListener('click', () => {
         this.activeList = false;
       });
@@ -84,16 +87,75 @@ export default {
     data(){
       return {
         activeList: false,
+        arrowKeys: [
+          38, 40
+        ],
+        list: null,
+        index: 0,
       }
     },
 
     methods: {
       openSelect(){
         this.activeList = !this.activeList;
+
+        this.index = this.getSelectedIndex();
+
+        this.putFocusInOption(this.index);
+
+        this.setArrowKeysEvent();
+      },
+
+      setArrowKeysEvent(){
+        const keydownHandlerEvent = this.keydownHandler;
+        this.$el.addEventListener('keydown', keydownHandlerEvent, false);
+      },
+
+      removeArrowKeysEvent(){
+        const keydownHandlerEvent = this.keydownHandler;
+        this.$el.removeEventListener('keydown', keydownHandlerEvent, false);
+      },
+
+      keydownHandler(){
+        if( this.arrowKeys.includes(event.keyCode) ){
+          event.preventDefault();
+          event.stopPropagation();
+
+          let index = this.index;
+
+          if( event.keyCode === 38 ){
+            const next = index - 1;
+
+            index = (next < 0) ? 0 : next;
+          }
+
+          if( event.keyCode === 40 ){
+            const next = index + 1;
+            const listSize = this.list.length -1;
+
+            index = (next > listSize) ? listSize : next;
+          }
+
+          this.putFocusInOption(index);
+        }
+      },
+
+      putFocusInOption(index){
+        this.list[index].focus();
+        this.index = index;
+      },
+
+      getSelectedIndex(){
+        const index = this.selectOptions.findIndex((option) => {
+          return this.selected.id === option.id;
+        });
+
+        return index;
       },
 
       changeChoosedOption(option){
         this.activeList = false;
+        this.removeArrowKeysEvent();
         this.$emit('change', option);
       },
     },
